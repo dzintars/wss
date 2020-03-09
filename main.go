933143a531833140105ca274941de03c12e461cd
Dzintars Klavins <dzintars.klavins@gmail.com>
2020-03-09 16:24:58 +0200

    chore(setup): Remove http endpint

--- main.go
+++ main.go
@@ -4,34 +4,14 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	"path/filepath"
-	"sync"
-	"text/template"
 )
-
-// templateHandler represents a single template
-type templateHandler struct {
-	once     sync.Once
-	filename string
-	templ    *template.Template
-}
-
-// ServeHTTP handles HTTP request.
-func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
-	})
-	t.templ.Execute(w, r)
-}
 
 func main() {
 	var addr = flag.String("addr", "8080", "The addr of the application.")
 	flag.Parse() // parse the flags
 
 	r := newRoom()
-	// r.tracer = trace.New(os.Stdout)
 
-	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/ws", r)
 
 	// get the room going
