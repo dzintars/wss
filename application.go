9cccd1fd363a3fffbc55624d77fce0f4b7ab8a4d
dzintars <dzintars.klavins@gmail.com>
2020-07-25 17:08:19 +0300

    chore(applications): rename uuid to id. fix #4

--- application.go
+++ application.go
@@ -6,7 +6,7 @@ package main
 
 // Application ...
 type Application struct {
-	UUID      string   `json:"uuid,omitempty"`
+	ID        string   `json:"id,omitempty"`
 	Title     string   `json:"title,omitempty"`
 	Component string   `json:"component,omitempty"`
 	Permalink string   `json:"permalink,omitempty"`
@@ -22,17 +22,17 @@ type Applications struct {
 var apps Applications = Applications{
 	Entities: map[string]Application{
 		"9a30119-d673-4978-b393-f608fe28ae07": {
-			UUID:      "9a30119-d673-4978-b393-f608fe28ae07",
+			ID:        "9a30119-d673-4978-b393-f608fe28ae07",
 			Title:     "Home",
 			Component: "app-home",
 			Permalink: "/",
 			Modules:   []string{},
 		},
 		"54789c07-bb43-4db4-8b2d-1a8e1f8c67f1": {
-			UUID:      "54789c07-bb43-4db4-8b2d-1a8e1f8c67f1",
+			ID:        "54789c07-bb43-4db4-8b2d-1a8e1f8c67f1",
 			Title:     "Dispatch",
 			Component: "app-users",
-			Permalink: "/apps/users",
+			Permalink: "/dispatch",
 			Modules: []string{
 				"9a84c3f2-c84b-4e44-b2b5-3ad9fa1840e4",
 				"502d3926-63a6-4cd5-a18d-01dfa6c64454",
@@ -44,28 +44,28 @@ var apps Applications = Applications{
 			},
 		},
 		"c178025e-a209-4c50-8c34-36d35f36494c": {
-			UUID:      "c178025e-a209-4c50-8c34-36d35f36494c",
+			ID:        "c178025e-a209-4c50-8c34-36d35f36494c",
 			Title:     "Sales",
 			Component: "app-users",
-			Permalink: "/apps/users/1",
+			Permalink: "/sales",
 			Modules: []string{
 				"12eb993e-3c0d-4c8a-b517-313b1225363f",
 			},
 		},
 		"437642dd-7d74-4213-af76-b51fc24eff0": {
-			UUID:      "437642dd-7d74-4213-af76-b51fc24eff0",
+			ID:        "437642dd-7d74-4213-af76-b51fc24eff0",
 			Title:     "Accounting",
 			Component: "app-users",
-			Permalink: "/apps/users/2",
+			Permalink: "accounting",
 			Modules: []string{
 				"7f7d075b-fb9f-46dc-b668-e561a753daed",
 			},
 		},
 		"5a2192a0-0051-46a1-85e7-17245ba24f55": {
-			UUID:      "5a2192a0-0051-46a1-85e7-17245ba24f55",
+			ID:        "5a2192a0-0051-46a1-85e7-17245ba24f55",
 			Title:     "Settings",
 			Component: "app-signin",
-			Permalink: "/signin",
+			Permalink: "/settings",
 			Modules: []string{
 				"20e60e8c-f86a-4522-855c-0e08f2c3bc58",
 			},
